cd51ac657ab59c06082d5c696a495d4e414f3885
SusanaSalmeron <exdream76@gmail.com>
2022-01-27 11:11:05 +0100

    doctors picture profile added

--- src/components/DoctorPicture/index.js
+++ src/components/DoctorPicture/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './doctorPicture.module.css'
-import img1 from '../Home/img1.jpg'
+
 
 
 
@@ -8,7 +8,7 @@ export default function DoctorPicture({ data }) {
     return (
         <>
             <div className={style.doctor}>
-                <img className={style.img} src={img1} alt="hospital img" />
+                <img className={style.img} src={data.photo} alt="photo_doctor" />
                 <h3 className={style.name}>{data.name}  <span className={style.speciality}>{data.speciality}</span></h3>
             </div>
         </>
