71e29e3947541d5d38effa07196788c100393654
SusanaSalmeron <exdream76@gmail.com>
2022-02-02 14:52:47 +0100

    popup alerts created

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import login from '../../services/loginService';
 import areYouAWorker from '../../middleware/checkWorker';
+import Swal from 'sweetalert2'
 
 
 export default function Login() {
@@ -26,8 +27,23 @@ export default function Login() {
         const { target: { email: { value: email }, password: { value: password } } } = evt
         const loginResult = await login(email, password)
         if (!loginResult.error) {
+            await Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'You are logged',
+                showConfirmButton: false,
+                timer: 2000
+            })
             areYouAWorker() ? navigate(`/${loginResult.id}/search`) : navigate(`/${loginResult.id}/appointment`)
+
         } else {
+            await Swal.fire({
+                position: 'center',
+                icon: 'error',
+                title: 'Login incorrect',
+                showConfirmButton: false,
+                timer: 2000
+            })
             setError(loginResult.error)
         }
     }
