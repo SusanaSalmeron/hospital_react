2d7f847243deec2b8355f9f209df4e681e4cdd1b
SusanaSalmeron <exdream76@gmail.com>
2022-02-03 13:23:03 +0100

    my account deleted and added modification data button instead

--- src/components/MyAppointment/index.js
+++ src/components/MyAppointment/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import NewAppointment from '../NewAppointment';
 import AppointmentList from '../AppointmentList';
 import Logout from '../Logout';
-import MyAccount from '../MyAccount';
+import ModifyDataButton from '../ModifyDataButton';
 import style from './myAppointment.module.css'
 import { getUser } from '../../services/appointmentService';
 import { useParams } from 'react-router-dom';
@@ -30,7 +30,7 @@ export default function MyAppointment() {
                 <div className={style.logout}>
                     <h1>Welcome, {user.name} </h1>
                     <h3>You can choose or cancel your appointments</h3>
-                    <MyAccount />
+                    <ModifyDataButton />
                     <Logout />
                 </div>
                 <hr />
