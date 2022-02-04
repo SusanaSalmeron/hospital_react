5895a7e97966efd862fec9c1a7e6f43fb5c4fcd5
SusanaSalmeron <exdream76@gmail.com>
2022-02-04 18:57:29 +0100

    footer logo updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import style from './footer.module.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import style from './footer.module.css';
+import { Link } from 'react-router-dom';
+import logo from './logo.png'
 
 
 export default function Footer() {
     return (
         <div className={style.container}>
             <div className={style.menu}>
-                <div className={style.logo}>H <span>H</span></div>
+                <div className={style.logo}>
+                    <Link to={"/home"}>
+                        <img src={logo} alt="logo" />
+                    </Link>
+                </div>
                 <ul className={style.ul}>
-
                     <li>
                         <Link to={"/legalNotice"}>
                             Legal Notice
