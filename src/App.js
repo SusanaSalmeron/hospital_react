a986b21cba24c03a335cf9cfbe8e2d64c1afe83b
SusanaSalmeron <exdream76@gmail.com>
2022-02-14 18:11:06 +0100

    scrolltotop automatically created

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import LegalNotice from './components/LegalNotice';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop'
 
 
 
@@ -27,25 +28,27 @@ function App() {
   return (
     <div className={style.App}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/:id/search" element={<PatientSearch />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/:id/record" element={<ClinicalRecord />} />
-          <Route path="/:id/addrecord" element={<RecordForm />} />
-          <Route path="/:id/appointment" element={<MyAppointment />} />
-          <Route path="/:id/modifyData" element={<ModificationData />} />
-          <Route path="/:id/appointListForDoctors" element={<AppointListForDoctors />} />
-          <Route path="/error403" element={<Error403 />} />
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/legalNotice" element={<LegalNotice />} />
-          <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
-          <Route path="/contactUs" element={<ContactUs />} />
-        </Routes>
-        <div className={style.footer}>
-          <Footer />
-        </div>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/:id/search" element={<PatientSearch />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/:id/record" element={<ClinicalRecord />} />
+            <Route path="/:id/addrecord" element={<RecordForm />} />
+            <Route path="/:id/appointment" element={<MyAppointment />} />
+            <Route path="/:id/modifyData" element={<ModificationData />} />
+            <Route path="/:id/appointListForDoctors" element={<AppointListForDoctors />} />
+            <Route path="/error403" element={<Error403 />} />
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/legalNotice" element={<LegalNotice />} />
+            <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
+            <Route path="/contactUs" element={<ContactUs />} />
+          </Routes>
+          <div className={style.footer}>
+            <Footer />
+          </div>
+        </ScrollToTop>
       </BrowserRouter>
 
     </div>
