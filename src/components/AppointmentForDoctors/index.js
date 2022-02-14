3317cf53d1922f11e91b2c188287dbf236ef8410
SusanaSalmeron <exdream76@gmail.com>
2022-02-14 14:38:37 +0100

    change doctor appointments style and button added

--- src/components/AppointmentForDoctors/index.js
+++ src/components/AppointmentForDoctors/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './appointmentForDoctors.module.css'
 
 
 
-
 export default function AppointmentForDoctors({ data }) {
+    const [newStyle, setNewStyle] = useState(false)
+    const [changeButton, setChangeButton] = useState(false)
+
+    const handleFinished = () => {
+        setNewStyle(!newStyle)
+        setChangeButton(!changeButton)
+    }
 
     return (
         <div className={style.showAppoint}>
-            <p>Date: <span>{data.pickedDate}</span>  Patient: <span >{data.patientName}</span></p>
+            {
+                !newStyle ?
+                    <p className={style.reactivate}>Date: <span>{data.pickedDate}</span>  Patient: <span >{data.patientName}</span></p> :
+                    <p className={style.finished}>Date: <span>{data.pickedDate}</span>  Patient: <span >{data.patientName}</span></p>
+            }
+
+            {
+                changeButton ?
+                    <button className={style.complete} onClick={handleFinished} >Unatteded</button> :
+                    <button onClick={handleFinished} >Attend</button>}
         </div>
     )
 }
