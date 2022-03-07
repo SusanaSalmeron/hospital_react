4d250d77e7518dcfa5e3e84e34ee0a2244a36afc
SusanaSalmeron <exdream76@gmail.com>
2022-03-07 17:12:26 +0100

    footer test

--- src/tests/footer.test.js
+++ src/tests/footer.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, getByText, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import React from 'react';
 import Footer from '../components/Footer';
@@ -8,13 +8,15 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 describe('Footer', () => {
     test("renders ok", async () => {
-        render(
+        const { container } = render(
             <Router>
                 <Footer />
             </Router>
         )
         expect(screen.getAllByRole("link")).toHaveLength(4)
-        /* await fireEvent.click(getByText('Contact Us')) */
+        expect(screen.getByAltText('logo')).toBeInTheDocument()
+        const p = container.querySelector('p')
+        expect(p).toContainHTML('2021 ©HopeHospital - All rights reserved')
 
     })
 })
