dca454739643f94e8b2951f2955946af2e61eb65
SusanaSalmeron <exdream76@gmail.com>
2022-03-11 15:16:16 +0100

    appointmentForDoctors test reactivated

--- src/tests/appointmentForDoctors.test.js
+++ src/tests/appointmentForDoctors.test.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AppointmentForDoctors from '../components/AppointmentForDoctors';
 
-/* const mockedData = {
+const mockedData = {
     id: 3,
     doctorId: 4,
     patientId: 1,
     patientName: "James Smith",
     pickedDate: "15/03/2022"
-} */
+}
 
 describe('AppointmentForDoctors', () => {
     test('renders ok', () => {
-        /* render(
+        render(
             <AppointmentForDoctors data={mockedData} />
         )
         expect(screen.getByText('James Smith')).toBeInTheDocument()
         expect(screen.getByText('15/03/2022')).toBeInTheDocument()
         expect(screen.getAllByRole('button')).toHaveLength(1)
-        expect(screen.getByRole('button')).toHaveAccessibleName('Done') */
+        expect(screen.getByRole('button')).toHaveAccessibleName('Done')
     })
 })
